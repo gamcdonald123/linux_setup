ca20ba6fc7ba73d03c6f10bc117bea79f4aa3ea9
gamcdonald123 <58171259+gamcdonald123@users.noreply.github.com>
2025-10-27 21:11:42 +0000

    zsh and ohmyzsh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "Initializing..."
+echo "[GUYS SETUP] Initializing..."
 sleep 1
 
-echo "Updating apt..."
+echo "[GUYS SETUP] Updating apt..."
 sudo apt update
 
+echo "[GUYS SETUP] Changing default shell to Zsh..."
+chsh -s $(which zsh)
+
+echo "[GUYS SETUP] Installing OhMyZsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
